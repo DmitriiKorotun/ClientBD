b822183ba57891efa4539633757416b4cc41387b
Dmitry <waild-cat@mail.ru>
2018-02-01 19:22:15 +0300

    Working on client-server interaction

---
+++ ClientBD/FormHandler/IWindowCreator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ClientBD.FormHandler
+{
+    public enum WindowType : int
+    {
+        COUNTRY = 0,
+        FIELD = 1,
+        PRODUCTION = 2,
+        PRODUCTION_DATA = 3,
+        PRODUCTION_TYPE = 4,
+        PROPERTY_TYPE = 5,
+        ROCK = 6,
+        ROCK_TYPE = 7
+    };
+
+    interface IWindowCreator
+    {
+        Window CreateWindow(WindowType windowType);
+    }
+}
